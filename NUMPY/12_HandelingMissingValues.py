a302c5de1f203133e983530da058012b16a2e36e
Aditya Sharma <adishahil346@gmail.com>
2025-04-02 21:01:35 +0530

    trimmed

--- NUMPY/12_HandelingMissingValues.py
+++ NUMPY/12_HandelingMissingValues.py
@@ -24,4 +24,4 @@ print(np.isinf(arr))
 +ve infinity and -ve infinity saperately
 '''
 cleand_arr = np.nan_to_num(arr,posinf=1000,neginf=-100)
-print(clean d_arr)
+print(cleand_arr)
--- NUMPY/SortFilterSearch.py
+++
@@ -1,2 +0,0 @@
-#how to preform sort filter and search opperatons on array
-# numpy.sort(array) will short the array in assending order
