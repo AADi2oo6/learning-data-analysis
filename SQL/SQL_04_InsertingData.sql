626c2769e7a26b82b6202dc09c5f126c223e72c5
Aditya Sharma <adishahil346@gmail.com>
2025-07-19 22:32:15 +0530

    inserting and StarterQuary

---
+++ SQL/SQL_04_InsertingData.sql
@@ -0,0 +1,17 @@
+-- > inserting single data
+-- INSERT INTO users
+-- VALUES (
+--     '2006-04-03',1,'adishahil346@gmail.com','Aditya Sharma','Male',DEFAULT);
+
+-- INSERT INTO users (name , email, gender,date_Of_birth)
+-- VALUES ("Rohan Sharma","Rohan123@gmail.com","Male","2005-12-03");
+
+-- inserting multiple data 
+-- INSERT INTO users (name, email, gender ,date_Of_birth) VALUES
+-- ("Soham Lokande","sohame23@gmail.com","male","2006-01-14"),
+-- ("Aditya Labhade","Adit123@gmail.com","male","2004-04-19"),
+-- ("Anisha Varma", "anish123@gmai.com","femail","2005-07-23");
+ 
+DESCRIBE USERS;
+
+
---
+++ SQL/SQL_05_StarterSql.sql
@@ -0,0 +1,2 @@
+use startersql;-- we have created table and inserted data using the starter sql scipts present in sartereQuary folder
+SELECT * from users;
